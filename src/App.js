6b78323bc3aac5753deefdf454df97ad1a91ced4
Bernz91 <samuel.bernadi91@gmail.com>
2022-06-11 18:34:40 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,64 @@ import React from "react";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: "",
+      toDoList: [],
+      completedTaskList: [],
+      isToggleOn: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.value.length > 0) {
+      this.setState({
+        toDoList: [...this.state.toDoList, this.state.value],
+      });
+    }
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
+
+  ArrayList(props) {
+    const items = props.toDoList;
+    const listItems = items.map((item) => (
+      <span>
+        <input type="checkbox" onClick={this.handleClick} />
+        {item}
+        <br />
+      </span>
+    ));
+    return listItems;
   }
 
   render() {
-    return;
+    return (
+      <div className="to-do-toDoList">
+        <h1>To-Do-List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+        <h2>Ongoing list</h2>
+        <this.ArrayList toDoList={this.state.toDoList} />
+        <h2>Completed List</h2>
+      </div>
+    );
   }
 }
